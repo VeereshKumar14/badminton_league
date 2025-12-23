09016f8df3f4cbc812d8f199461bff60097e063d
VeereshKumar14 <rveereshkumarr@gmail.com>
2025-12-23 15:19:42 +0530

    initial project setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# Badminton League – Ruby on Rails Application
+
+A simple Ruby on Rails web application to manage a badminton league.
+It allows users to manage players, record match results with scores, and view a leaderboard ranked by wins.
+
+## Features
+### Core Features
+
+- Add and remove players
+- Record matches between players
+- Store match scores (winner & loser)
+- Automatically track wins and losses
+- Display leaderboard sorted by wins
+
+## Tech Stack
+
+- Ruby 3.4.3
+- Rails 8.1.1
+- HTML / ERB – Views
---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2025_12_23_093810) do
+  create_table "matches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "loser_id", null: false
+    t.integer "loser_score"
+    t.datetime "updated_at", null: false
+    t.integer "winner_id", null: false
+    t.integer "winner_score"
+    t.index ["loser_id"], name: "index_matches_on_loser_id"
+    t.index ["winner_id"], name: "index_matches_on_winner_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "matches", "losers"
+  add_foreign_key "matches", "winners"
+end
